529ecc0d4fe186a871ea59eeee3533df77e58325
Liza Phan <phantr98@gmail.com>
2020-02-26 20:31:03 +0300

    Automatic redirect from index to dashboard (commented)

--- again/views.py
+++ again/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 # Create your views here.
 def index(request):
+    # Uncomment following 2 lines for automatic redirects
+    # from index page to dashboard for authorized users (for Kirill)
+    # if request.user.is_authenticated:	# TODO: replace with something more beautiful
+    #     return HttpResponseRedirect('/finances/dashboard')
     template = loader.get_template('again/index.html')
     return HttpResponse(template.render({}, request))
